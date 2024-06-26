610fe2ccf266cb1be35583924cc6cd4cbe18e9f9
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-06-26 17:38:43 +0530

    modified

--- basic-twitter-frontend/src/components/Login.jsx
+++ basic-twitter-frontend/src/components/Login.jsx
@@ -26,6 +26,10 @@ const Login = () => {
       const response = await axios.post("https://socio-psi.vercel.app/login", {
         userName: userName,
         password: password,
+      },{
+        headers: {
+          'Acess-Control-Allow-Origin': 'https://socio-psi.vercel.app'
+        }
       })
       console.log(response.data.token);
       localStorage.setItem('token', response.data.token);
