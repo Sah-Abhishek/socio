633edbc68ad5e575eb0f1f1940937f8e93dc246e
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-06-26 22:27:25 +0530

    modified

--- basic-twitter-backend/index.js
+++ basic-twitter-backend/index.js
@@ -19,12 +19,12 @@ connectMongoDB();
 
 const app = express();
 app.use(cors(
-    // {
-    //     origin: ["https://socio-cli-abhisheks-projects-3a5a3785.vercel.app"],
-    //     methods: ['POST', 'GET'],
-    //     credentials: true,
-    //     allowedHeaders: ['Content-Type', 'Authorization'],
-    // }
+    {
+        origin: ["https://socio-cli-abhisheks-projects-3a5a3785.vercel.app"],
+        methods: ['POST', 'GET'],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }
 ));
 
 /*
